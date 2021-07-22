4e1c7febadf2d3206eb69f2750eb8760a7b26c3f
ekisshu <46841693+ekisshu@users.noreply.github.com>
2021-07-22 19:53:26 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-terraform {
+terraform {                               #Terraform block for terraform setting
   required_providers {
     aws = {
       source  = "hashicorp/aws"
@@ -9,16 +9,19 @@ terraform {
   required_version = ">= 0.14.9"
 }
 
-provider "aws" {
+provider "aws" {                           #Provider block configures the specified provider, in this case aws
   profile = "default"
-  region  = "ap-south-1"        ## for general region =${var.aws_region} 
-}
+  region  = "ap-south-1"          
 
-resource "aws_instance" "Assignment" {
+resource "aws_instance" "Assignment" {     # resource blocks to define components of your infrastructure
   ami           = "ami-00bf4ae5a7909786c"
-  instance_type = "t2.micro"
+  instance_type = "${var.intance_type}"    # instance_type vaule taken from var.tf file
+}
 
   tags = {
     Name = "NTTData"
   }
 }
+
+
+
